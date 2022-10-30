777da9de3569ab066b769ce879ebc2b1bf698af7
LHC <lautarohconti@gmail.com>
2022-10-30 20:04:13 -0300

    Nuevos enlaces con el back SignUp ForgotPassword ConfirmEmail

--- screens/NewPasswordScreen/NewPasswordScreen.js
+++ screens/NewPasswordScreen/NewPasswordScreen.js
@@ -1,20 +1,42 @@
 import React from 'react'
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Alert } from 'react-native'
 import CustomButton from '../../src/components/CustomButton/CustomButton'
 import CustomInput from '../../src/components/CustomInput/CustomInput'
 import { useNavigation } from '@react-navigation/native'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import utils from '../../src/utils/utilities'
+import { useSelector } from 'react-redux'
 
 export default function NewPasswordScreen () {
   // const [code,setCode]=useState('')
   // const [password,setPassword]=useState('')
   // const [repeatPassword,setRepeatPassword]=useState('')
-
+  const user = useSelector(state => state.user)
   const navigation = useNavigation()
-  const { control, handleSubmit } = useForm()
+  const { control, handleSubmit, watch } = useForm({
+    defaultValues: {
+      username: 'ewines'
+    }
+  })
+  const pwd = watch('password')
 
-  const send = () => {
-    navigation.navigate('Home')
+  const send = async (newPasswordData) => {
+    if (user) {
+      try {
+        const res = await axios.post(`${utils.URLAPI}/users/${user.id}`, newPasswordData)
+        console.log(res)
+        if (res.data) {
+          navigation.navigate('SignIn')
+        } else {
+          Alert('Error', 'No se pudo cambiar la contraseña')
+        }
+      } catch (error) {
+        console.log(error.message)
+      }
+    } else {
+      Alert('Error', 'No hay usuario')
+    }
   }
 
   const goToSignIn = () => {
@@ -26,9 +48,11 @@ export default function NewPasswordScreen () {
       <View style={styles.root}>
         <Text style={styles.title}>Nueva contraseña</Text>
 
-        <CustomInput name='code' control={control} placeholder='Ingrese el código que le enviamos por email' />
-        <CustomInput name='newPassword' control={control} placeholder='New password' secureTextEntry />
-        <CustomInput name='repeatNewPassword' control={control} placeholder='Repeat new password' secureTextEntry />
+        <CustomInput name='password' control={control} placeholder='Ingrese la contraseña provisoria que le enviamos por email' />
+        <CustomInput
+          name='password2' control={control} placeholder='New password' secureTextEntry rules={{ required: 'Se requiere password', minLength: { value: 8, message: 'Password debe tener al menos 8 caracteres' }, maxLength: { value: 20, message: 'Password solo puede tener 20 caracteres como máximo' } }}
+        />
+        <CustomInput name='passwordRepeat' control={control} placeholder='Repeat new password' secureTextEntry rules={{ validate: value => value === pwd || 'Las contraseñas no coinciden' }} />
 
         <CustomButton onPress={handleSubmit(send)} text='Registrar nueva contraseña' />
 
